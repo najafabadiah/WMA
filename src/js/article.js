ce82bf1ef0f3a43af0a8631afa81f3abfa24df1a
Bogna Manowska <bogna.manowska@gmail.com>
2021-05-19 14:26:51 +0200

    done everything besides working navigation menu

--- src/js/article.js
+++ src/js/article.js
@@ -1,1 +1,11 @@
+const root = document.querySelector("#root");
+const fontIncreaseBtn = document.querySelector("#font-increase-button");
+const fontDecreaseBtn = document.querySelector("#font-decrease-button");
 
+fontIncreaseBtn.addEventListener("click", function(){
+	root.style.fontSize = "150%";
+});
+
+fontDecreaseBtn.addEventListener("click", function(){
+	root.style.fontSize = "100%";
+});
